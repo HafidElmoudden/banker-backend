20ee1ce0adbc4ce1eb5252a355e6cbf48fd49094
HafidElmoudden <abdulhafid858@gmail.com>
2025-05-17 20:37:32 +0000

    Init project & Implemented entities

---
+++ src/main/java/com/hafidelmoudden/bankerbackend/entities/AccountOperation.java
@@ -0,0 +1,24 @@
+package com.hafidelmoudden.bankerbackend.entities;
+
+import com.hafidelmoudden.bankerbackend.enums.OperationType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.util.Date;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class AccountOperation {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date operationDate;
+    private double amount;
+    @Enumerated(EnumType.STRING)
+    private OperationType type;
+    @ManyToOne
+    private BankAccount bankAccount;
+    private String description;
+}
+
---
+++ src/main/java/com/hafidelmoudden/bankerbackend/enums/AccountStatus.java
@@ -0,0 +1,5 @@
+package com.hafidelmoudden.bankerbackend.enums;
+
+public enum AccountStatus {
+    CREATED, ACTIVATED, SUSPENDED
+}
